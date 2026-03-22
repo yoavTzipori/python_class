944c6c53af168c53e6dcaeb333641c3303480ba2
yoavTzipori <112960366+yoavTzipori@users.noreply.github.com>
2026-03-22 19:47:31 +0200

    Update app.py
    test-yoav

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Jenkins + AWS ECR Pipeline!"
+    return "hello from yoav-test"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
